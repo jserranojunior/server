b3b5e7eeadb02ebe160227ff26619fced3fe4ad3
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2023-02-01 19:11:14 -0300

    production

--- backup/scripts/mysql_production.sh
+++ backup/scripts/mysql_production.sh
@@ -1,5 +1,5 @@
 DATE=`date +%d-%m-%Y:%H:%M:%S`
 docker exec mysql_prod /usr/bin/mysqldump -u root --password=@1Mysql mysql > /export/backup/bancodedados/production/$DATE-mysql.backup.sql
 docker exec mysql_prod /usr/bin/mysqldump -u root --password=@1Mysql acolher > /export/backup/bancodedados/production/$DATE-acolher.backup.sql
-docker exec mysql_prod /usr/bin/mysqldump -u root --password=@1Mysql intellect > /export/backup/bancodedados/production/$DATE-intellec.backup.sql
-docker exec mysql_prod /usr/bin/mysqldump -u root --password=@1Mysql nextcloud > /export/backup/bancodedados/production/$DATE-intellec.backup.sql
+<# docker exec mysql_prod /usr/bin/mysqldump -u root --password=@1Mysql intellect > /export/backup/bancodedados/production/$DATE-intellec.backup.sql
+ #>docker exec mysql_prod /usr/bin/mysqldump -u root --password=@1Mysql nextcloud > /export/backup/bancodedados/production/$DATE-nextcloud.backup.sql
